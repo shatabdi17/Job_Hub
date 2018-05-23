aad522421de009bfbef8379b0f4ace97fdc8636c
Corey Lanier <iam@coreylanier.com>
2018-05-23 17:39:11 -0400

    api calls made, pushed to state

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Firebase from './firebase';
+import firebase from 'firebase';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
 
 
 // Initialize Firebase
@@ -14,16 +21,47 @@ const config = {
 };
 
 
+
 firebase.initializeApp(config);
 
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
-    }
+  constructor() {
+    super();
+    this.state = {
+      location: 'Toronto',
+      jobs: []
+    };
+  }
+  componentDidMount() {
+    axios.get(
+      "https://cors-anywhere.herokuapp.com/api.indeed.com/ads/apisearch",
+      {
+        params: {
+          publisher: "2117056629901044",
+          v: 2,
+          format: "json",
+          q: "Marketing",
+          l: this.state.locationToSearch,
+          co: "ca",
+
+          start: this.state.currentPage * 10,
+          limit: 10
+        }
+      }
+    ).then((res) => {
+      console.log(res);
+      this.setState({
+        jobs: res.data.results
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
